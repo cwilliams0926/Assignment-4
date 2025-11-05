cc3e5668290b93bc26ebfa46ab6634c50c45f166
cwilliams0926 <coopwilliams03@hotmail.com>
2025-11-05 18:16:55 -0500

    Add Card class

---
+++ Assignment 3/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_3
+{
+    public class Card
+    {
+        public int Id { get; }
+
+        public Card(int id)
+        {
+            Id = id;
+        }
+    }
+}
