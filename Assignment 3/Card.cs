d66e4924a61c03ae079a2bc481f8a1e2f0cbcf89
cwilliams0926 <coopwilliams03@hotmail.com>
2025-11-08 21:55:18 -0500

    Add Name property to Card class and update constructor

--- Assignment 3/Card.cs
+++ Assignment 3/Card.cs
@@ -10,17 +10,19 @@ namespace Assignment_3
     {
         public int Id { get; }
         public Image? CardImage { get; }
-        public static readonly Card NoCard = new Card(-1, null);
+        public static readonly Card NoCard = new Card(-1, null, "No Card");
+        public string Name { get; }
 
-        public Card(int id, Image? image)
+        public Card(int id, Image? image, string name)
         {
             Id = id;
             CardImage = image;
+            Name = name;
         }
 
         public override string ToString()
         {
-            return Id.ToString();
+            return Name;
         }
     }
 }
